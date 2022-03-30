add35e267a284a4d56f948ad6d5e9d8afe5090f2
mhrwh <i072ff@yamaguchi-u.ac.jp>
2022-03-31 00:00:53 +0900

    不要なコードを削除

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,6 @@ import {
     createConnection,
     TextDocuments,
     ProposedFeatures,
-    InitializeParams,
-    DidChangeConfigurationNotification,
     TextDocumentPositionParams,
     TextDocumentSyncKind,
     InitializeResult,
@@ -44,28 +42,10 @@ export const connection = createConnection(ProposedFeatures.all);
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
-let hasConfigurationCapability = false;
-let hasWorkspaceFolderCapability = false;
-
-connection.onInitialize((params: InitializeParams) => {
-    const capabilities = params.capabilities;
-
-    // Does the client support the `workspace/configuration` request?
-    // If not, we fall back using global settings.
-    hasConfigurationCapability = !!(
-        capabilities.workspace && !!capabilities.workspace.configuration
-    );
-    hasWorkspaceFolderCapability = !!(
-        capabilities.workspace && !!capabilities.workspace.workspaceFolders
-    );
-
+connection.onInitialize(() => {
     const result: InitializeResult = {
         capabilities: {
             textDocumentSync: TextDocumentSyncKind.Incremental,
-            // Tell the client that this server supports code completion.
-            completionProvider: {
-                resolveProvider: true
-            },
             executeCommandProvider: {
                 commands: [
                     MIZAR_COMMANDS.mizar_verify,
@@ -85,26 +65,7 @@ connection.onInitialize((params: InitializeParams) => {
             definitionProvider: true
         }
     };
-    if (hasWorkspaceFolderCapability) {
-        result.capabilities.workspace = {
-            workspaceFolders: {
-                supported: true
-            }
-        };
-    }
     return result;
-});
-
-connection.onInitialized(() => {
-    if (hasConfigurationCapability) {
-        // Register for all configuration changes.
-        connection.client.register(DidChangeConfigurationNotification.type, undefined);
-    }
-    if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders(_event => {
-            connection.console.log('Workspace folder change event received.');
-        });
-    }
 });
 
 export function showMessage(
